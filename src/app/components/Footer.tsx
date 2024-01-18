1c5559684a67eeea44f9b2bc7912f28fc814162f
Guilherme Paes <paes.guilherme@hotmail.com>
2024-01-18 08:40:46 -0300

    :sparkles: feat: finish footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
+import logo from "../assets/logo-footer.svg";
+
 export default function Footer() {
   return (
-    <footer className='flex flex-col py-14 px-4 mobile:px-8 tablet:px-36 max-w-[1152px] mx-auto mobile:flex-row'>
-      <div className='flex justify-between flex-wrap'>
-        <h1>logo</h1>
-        <div className='flex gap-6 bg-blue-900 flex-wrap'>
-          <section className='flex flex-col gap-6'>
+    <footer className='flex flex-col py-14 px-4 mobile:px-8 tablet:px-36 mx-auto '>
+      <div className='flex justify-between flex-wrap items-start mb-12'>
+        <Image src={logo} alt='logo' className='mb-14' />
+        <div className='flex gap-6 flex-wrap items-start max-w-[858px]'>
+          <section className=' w-[172px] flex flex-col gap-6 mb-12'>
             <h3 className='text-slate-100 text-xl font-bold'>Cursos</h3>
             <a href='#' className='text-slate-100 text-base font-medium'>
               Valuation do Zero ao Avançado 2.0
@@ -19,7 +22,7 @@ export default function Footer() {
               Enciclopédia de FII
             </a>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className=' w-[172px] flex flex-col gap-6 mb-12'>
             <h3 className='text-slate-100 text-xl font-bold'>Carteiras</h3>
             <a href='#' className='text-slate-100 text-base font-medium'>
               Carteira FATOR
@@ -40,13 +43,13 @@ export default function Footer() {
               Carteira de FIIs
             </a>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className=' w-[172px] flex flex-col gap-6'>
             <h3 className='text-slate-100 text-xl font-bold'>Sobre</h3>
             <a href='#' className='text-slate-100 text-base font-medium'>
               Quem somos
             </a>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className=' w-[172px] flex flex-col gap-6'>
             <h3 className='text-slate-100 text-xl font-bold'>Redes Sociais</h3>
             <a href='#' className='text-slate-100 text-base font-medium'>
               Instagram
