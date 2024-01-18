874c3faf50594c958790194b66fe7a24dcda9293
Guilherme Paes <paes.guilherme@hotmail.com>
2024-01-18 07:55:32 -0300

    :sparkles: feat: add new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import SectionI from "./sections/Section1";
 
 export default function Home() {
   return (
     <main className='min-h-[100vh] w-full overflow-x-hidden'>
       <Header />
+      <SectionI />
     </main>
   );
 }
