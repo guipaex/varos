b4e9f06655f7a128cdc6c87e0d2d8f62b1c238c9
Guilherme Paes <paes.guilherme@hotmail.com>
2024-01-19 17:47:33 -0300

    :sparkles: feat: add contact section

---
+++ src/app/Sections/Contact/index.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <section>
+      <div>
+        <h2>Cadastre-se para receber mais informações.</h2>
+        <h3>Fique ligado no que tem de melhor no Mercado Financeiro.</h3>
+      </div>
+      <form></form>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from "./Sections/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className='min-h-[100vh] w-full overflow-x-hidden'>
       <Header />
+      <Contact />
       <Footer />
     </main>
   );
